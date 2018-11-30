e7ba3560ca4a73e8656ea14da890dad439160112
Jeferson Shin <shin.jef@gmail.com>
2018-11-29 22:38:31 -0200

    comandos básicos gerado, inclusão de regras para saber o tamanho da tela e usar isso para não criar mosquitos fora do tamanho da tela.

---
+++ jogo.js
@@ -0,0 +1,33 @@
+//TAMANHO DA TELA DO JOGO
+var altura = window.innerHeight
+var largura = window.innerWidth
+
+function ajudaTamanhoPalco() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+}
+
+ajudaTamanhoPalco()
+
+
+
+//GERACAO DE POSICAO RANDOMICA
+var posicaoX = Math.floor( (Math.random() * largura) ) - 90
+var posicaoY = Math.floor( (Math.random() * altura) ) - 90
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+
+
+//CRIACAO DOS ELEMENTOS
+function posicaoRandomica() {
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosca.png'
+	mosquito.className = 'mosquito1'
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+	
+	document.body.appendChild(mosquito)
+}
