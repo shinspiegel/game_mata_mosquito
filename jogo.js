563bb0c7db92d275a575a48d2583ec95aeb16c8b
Admin <Admin@Mac-mini-de-Carlos.local>
2018-11-30 12:57:24 -0200

    coracao da aplicaçao feito, elementos surgem e desaparecem

--- jogo.js
+++ jogo.js
@@ -10,24 +10,55 @@ function ajudaTamanhoPalco() {
 ajudaTamanhoPalco()
 
 
-
-//GERACAO DE POSICAO RANDOMICA
-var posicaoX = Math.floor( (Math.random() * largura) ) - 90
-var posicaoY = Math.floor( (Math.random() * altura) ) - 90
-
-posicaoX = posicaoX < 0 ? 0 : posicaoX
-posicaoY = posicaoY < 0 ? 0 : posicaoY
-
-
-
 //CRIACAO DOS ELEMENTOS
 function posicaoRandomica() {
+	
+	//remover mosquito se ele existe
+	if ( document.getElementById('mosquito') ) {
+		document.getElementById('mosquito').remove()
+	}
+
+	//criando posicao alteatoria
+	var posicaoX = Math.floor( Math.random() * largura ) - 90
+	var posicaoY = Math.floor( Math.random() * altura ) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	//criando elemento
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosca.png'
-	mosquito.className = 'mosquito1'
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
+	mosquito.id = 'mosquito'
 	
+	//inserindo elemento
 	document.body.appendChild(mosquito)
+}
+
+
+
+//TAMANHO ALEATORIO
+function tamanhoAleatorio() {
+	var classe = Math.floor( Math.random() * 3 )
+
+	switch(classe) {
+		case 0: return 'mosquito1'
+		case 1: return 'mosquito2'
+		case 2: return 'mosquito3'
+	}
+}
+
+
+
+//LADO ALEATORIO
+function ladoAleatorio() {
+	var classe = Math.floor( Math.random() * 2 )
+
+	switch(classe) {
+		case 0: return 'ladoA'
+		case 1: return 'ladoB'
+	}
 }
